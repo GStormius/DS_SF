c36061a6b65078f271f54a0a60194d1cec73666c
Nikita Popov <mr.nikita.popov97@gmail.com>
2021-12-04 21:29:14 +0300

    Добавлен итоговый проект

---
+++ PYTHON_8_6/game_v3.py
@@ -0,0 +1,33 @@
+from game_v2 import score_game
+
+def game_core_v3(number: int = 1) -> int:
+    """
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+
+    count = 0   # счетчик попыток
+    predict_number_min = 1  # нижняя граница поиска числа
+    predict_number_max = 101  # верхняя граница поиска числа
+    
+    while True:
+        count+=1
+        
+        predict_number = (predict_number_max + predict_number_min)//2
+
+        if number > predict_number:
+            predict_number_min = predict_number  # смещение нижней границы поиска числа
+
+        elif number < predict_number:
+            predict_number_max = predict_number  # смещение верхней границы поиска числа
+
+        else:
+            break
+
+    return count
+
+
+print(score_game(game_core_v3))
